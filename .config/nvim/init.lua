90097fd0caf07cf9c5e352d40d853f986cb30605
d0nu7 <65747512+D0NU7-H0G@users.noreply.github.com>
2026-07-21 17:24:36 +0800

    add lazynvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,12 @@
+vim.opt.clipboard = "unnamedplus"
+
 vim.api.nvim_create_autocmd("VimEnter", { -- opens nvimtree on start
 	callback = function()
 		vim.cmd("NvimTreeOpen")
 	end,
 })
+
+-- Theme --
 
 vim.pack.add({
 	{
@@ -18,6 +22,8 @@ require("rose-pine").setup({
 	extend_background_behind_borders = true,
 })
 vim.cmd("colorscheme rose-pine")
+
+-- === PLUGINS === --
 
 vim.pack.add({
     'https://github.com/nvim-treesitter/nvim-treesitter',
@@ -74,3 +80,39 @@ vim.pack.add({
   }
   require("nvim-tree").setup(config)
 
+-- === LAZYNVIM === --
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- add your plugins here
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
