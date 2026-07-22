24b84faa2f8e6239305b6ff32f72d5a9c8354355
d0nu7 <65747512+D0NU7-H0G@users.noreply.github.com>
2026-07-22 17:46:14 +0800

    nvim: fix nvimtree open on start, config spacing

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,48 +1,42 @@
 vim.opt.clipboard = "unnamedplus"
 
-vim.api.nvim_create_autocmd("VimEnter", { -- opens nvimtree on start
-	callback = function()
-		vim.cmd("NvimTreeOpen")
-	end,
-})
-
 -- Theme --
 
 vim.pack.add({
-	{
-		src = "https://github.com/rose-pine/neovim",
-		name = "rose-pine",
-	},
+  {
+    src = "https://github.com/rose-pine/neovim",
+    name = "rose-pine",
+  },
 })
 
 require("rose-pine").setup({
-	variant = "auto",
-	dark_variant = "moon",
-	dim_inactive_windows = false,
-	extend_background_behind_borders = true,
+  variant = "auto",
+  dark_variant = "moon",
+  dim_inactive_windows = false,
+  extend_background_behind_borders = true,
 })
 vim.cmd("colorscheme rose-pine")
 
 -- === PLUGINS === --
 
 vim.pack.add({
-    'https://github.com/nvim-treesitter/nvim-treesitter',
-    'https://github.com/nvim-mini/mini.nvim',            -- if you use the mini.nvim suite
-    -- 'https://github.com/nvim-mini/mini.icons',        -- if you use standalone mini plugins
-    -- 'https://github.com/nvim-tree/nvim-web-devicons', -- if you prefer nvim-web-devicons
-    'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+  'https://github.com/nvim-treesitter/nvim-treesitter',
+  'https://github.com/nvim-mini/mini.nvim',            -- if you use the mini.nvim suite
+  -- 'https://github.com/nvim-mini/mini.icons',        -- if you use standalone mini plugins
+  -- 'https://github.com/nvim-tree/nvim-web-devicons', -- if you prefer nvim-web-devicons
+  'https://github.com/MeanderingProgrammer/render-markdown.nvim',
 })
 require('render-markdown').setup({
-    latex = {
-        enabled = true,
-        render_modes = false,
-        converter = { 'utftex', 'latex2text' },
-        inline = true,
-        block = true,
-        highlight = 'RenderMarkdownMath',
-        position = 'center',
-        top_pad = 0,
-        bottom_pad = 0,
+  latex = {
+    enabled = true,
+    render_modes = false,
+    converter = { 'utftex', 'latex2text' },
+    inline = true,
+    block = true,
+    highlight = 'RenderMarkdownMath',
+    position = 'center',
+    top_pad = 0,
+    bottom_pad = 0,
     },
 })
 
@@ -61,24 +55,12 @@ vim.pack.add({
   -- empty setup using defaults
   require("nvim-tree").setup()
 
-  -- OR setup with a config
 
-  ---@type nvim_tree.config
-  local config = {
-    sort = {
-      sorter = "case_sensitive",
-    },
-    view = {
-      width = 30,
-    },
-    renderer = {
-      group_empty = true,
-    },
-    filters = {
-      dotfiles = true,
-    },
-  }
-  require("nvim-tree").setup(config)
+vim.api.nvim_create_autocmd("VimEnter", { -- opens nvimtree on start
+  callback = function()
+    require("nvim-tree.api").tree.open()
+  end,
+})
 
 -- === LAZYNVIM === --
 
