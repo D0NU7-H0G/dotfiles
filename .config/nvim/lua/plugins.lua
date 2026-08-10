0e247f857ba6348322b107aaff3d7d744c69314c
d0nu7 <65747512+D0NU7-H0G@users.noreply.github.com>
2026-08-11 00:47:52 +0800

    nvim: separate plugins into own file

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,41 @@
+-- === PLUGINS === --
+
+-- Nvim treesitter --
+vim.pack.add({
+  'https://github.com/nvim-treesitter/nvim-treesitter',
+  'https://github.com/nvim-mini/mini.nvim',            -- if you use the mini.nvim suite
+  -- 'https://github.com/nvim-mini/mini.icons',        -- if you use standalone mini plugins
+  -- 'https://github.com/nvim-tree/nvim-web-devicons', -- if you prefer nvim-web-devicons
+  'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+})
+-- Nvim render markdown --
+require('render-markdown').setup({
+  latex = {
+    enabled = true,
+    render_modes = false,
+    converter = { 'utftex', 'latex2text' },
+    inline = true,
+    block = true,
+    highlight = 'RenderMarkdownMath',
+    position = 'center',
+    top_pad = 0,
+    bottom_pad = 0,
+    },
+})
+
+-- Nvim tree --
+vim.pack.add({
+  { src = 'https://github.com/nvim-tree/nvim-web-devicons' }, -- optional
+  { src = 'https://github.com/nvim-tree/nvim-tree.lua' },
+})
+
+  -- disable netrw at the very start of your init.lua
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
+
+  -- optionally enable 24-bit colour
+  vim.opt.termguicolors = true
+
+  -- empty setup using defaults
+  require("nvim-tree").setup()
+
