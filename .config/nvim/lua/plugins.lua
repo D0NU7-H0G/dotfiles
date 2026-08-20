a19ff3b09c997c1c8a63a0097f64508178e2bcf3
d0nu7 <65747512+D0NU7-H0G@users.noreply.github.com>
2026-08-20 11:52:25 +0800

    nvim: add fff plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -53,3 +53,23 @@ vim.pack.add({
   -- empty setup using defaults
   require("nvim-tree").setup()
 
+
+-- fff (file search) --
+vim.pack.add({ 'https://github.com/dmtrKovalenko/fff' })
+
+vim.api.nvim_create_autocmd('PackChanged', {
+  callback = function(ev)
+    local name, kind = ev.data.spec.name, ev.data.kind
+    if name == 'fff' and (kind == 'install' or kind == 'update') then
+      if not ev.data.active then vim.cmd.packadd('fff') end
+      require('fff.download').download_or_build_binary()
+    end
+  end,
+})
+
+vim.g.fff = {
+  lazy_sync = true,
+  debug = { enabled = true, show_scores = true },
+}
+
+vim.keymap.set('n', 'ff', function() require('fff').find_files() end, { desc = 'FFFind files' })
